f61e5a0096e48b100ec45f66f6c958cb63ec7682
Rizal Ardhi Rahmadani <rizal.ardhi.rahmadani@gmail.com>
2019-10-11 20:09:50 +0700

    init

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
-Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+Route::any('logout', 'Auth\LoginController@logout')->name('logout');
 
 
 Route::get('/', 'HomeController@index')->name('home');
